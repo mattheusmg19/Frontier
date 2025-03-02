9e9e1105bd7f7f4392eb36d7ae373527b1ff32bc
mattheusmg19 <matheus.graciano@grendene.com.br>
2025-03-02 14:07:40 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado.');
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado!");
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', (event) => {
-    console.log('Interceptando request para:', event.request.url);
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker ativado!");
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Interceptando requisição para:", event.request.url);
 });
